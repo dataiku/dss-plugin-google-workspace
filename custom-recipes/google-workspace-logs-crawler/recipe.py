ae6b237d8bfaf38d0ac74c2ff74d93f5f876409c
Matthieu Bouthors <matthieu.bouthors@dataiku.com>
2024-03-07 08:56:38 +0100

    Avoid error if dataset not partionned

--- custom-recipes/google-workspace-logs-crawler/recipe.py
+++ custom-recipes/google-workspace-logs-crawler/recipe.py
@@ -3,9 +3,15 @@ import dataiku
 from dataiku.customrecipe import get_recipe_config, get_output_names_for_role
 from google_admin_reports import get_activities_as_dataframe
 from google.oauth2.credentials import Credentials
+from datetime import date
 
 creds = Credentials(get_recipe_config()['oauth']['oauth_credentials'])
-logs_df = get_activities_as_dataframe(creds, get_recipe_config()['application_name'], dataiku.dku_flow_variables["DKU_DST_date"])
+date = date.today()
+try:
+  date = dataiku.dku_flow_variables["DKU_DST_date"]
+except:
+  print('Dataset does not seem to be partionned.')
+logs_df = get_activities_as_dataframe(creds, get_recipe_config()['application_name'], date)
 
 if not logs_df.empty:
     output_A_name = get_output_names_for_role('main_output')[0]
